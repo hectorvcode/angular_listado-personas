743b5f770f4c94034e7f3ca8bda0a7e9d12dc505
hectorvcode <hectorfabiovasquez@gmail.com>
2021-10-27 20:35:51 -0500

    Finished App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,16 @@ import { Persona } from './persona.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'listado-personas';
+  titulo = 'Listado de Personas';
   personas: Persona[] = [
     new Persona('Juan','Perez'),
     new Persona('Laura','Juarez'),
     new Persona('Hector','Vasquez')
   ];
+  nombreInput:string = '';
+  apellidoInput:string = '';
+  agregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+  }
 }
