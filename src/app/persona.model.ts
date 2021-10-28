1d2545b22b1874660cc413d6e0f725324dc40ec8
hectorvcode <hectorfabiovasquez@gmail.com>
2021-10-27 19:55:11 -0500

    Created personas array with two new objects and class Persona with constructor

---
+++ src/app/persona.model.ts
@@ -0,0 +1,12 @@
+export class Persona {
+    // nombre:string = '';
+    // apellido:string = '';
+
+    // constructor(nombre:string, apellido:string){
+    //     this.nombre = nombre;
+    //     this.apellido = apellido;
+    // }
+
+    /**Con esta sintaxis abreviada nos ahorramos el codigo de arriba */
+    constructor(public nombre:string, public apellido:string){}
+}
